62e726724df215045e3be072a6f307eaf5ee91b3
Anupong Pengsawang <41697381+NicEKuN@users.noreply.github.com>
2021-09-15 08:24:22 +0700

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,139 @@
+import argparse
+
+from clip import Clip
+
+from pathlib import Path
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--input", "-i", help="Path to the input video", type=Path, required=True
+    )
+    parser.add_argument(
+        "--output",
+        "-o",
+        help="Path to where you want to save the output video. "
+             "Note: Not all extensions are supported. Checkout moviepy's documentation",
+        type=Path,
+        required=True,
+    )
+    parser.add_argument(
+        "--magnitude-threshold-ratio",
+        "-m",
+        help="Audio signal's values ({x in R: -1 <= x <= 1}) that are: "
+             "min_magnitude = min(abs(min(x)), max(x))"
+             "magnitude_threshold = min_magnitude * magnitude_threshold_ratio"
+             "abs(x) < magnitude_threshold"
+             "will be threated as silence audio signal values.",
+        type=float,
+        default=0.02,
+    )
+    parser.add_argument(
+        "--duration-threshold",
+        "-d",
+        help="Minimum number of required seconds in silence to cut it out",
+        type=float,
+        default=0.5,
+    )
+    parser.add_argument(
+        "--failure-tolerance-ratio",
+        "-f",
+        help="Consequent x values are taken into account to find silence parts of the signal. "
+             "Failure tolerance ratio leaves room for some error. For example if failure threshold "
+             "ratio is 0.1, then in 1 second silence signal, it is allowed to have 0.1 seconds "
+             "non silence signal, and it is still going to be treated as a silence signal.",
+        type=float,
+        default=0.1,
+    )
+    parser.add_argument(
+        "--space-on-edges",
+        "-s",
+        help="Leaves some space on the edges of silence cut. E.g. if it is found that there is silence "
+             "between 10th and 20th second of the video, then instead of cutting it out directly, "
+             "we cut out  (10+space_on_edges)th and (20-space_on_edges)th seconds of the clip",
+        type=float,
+        default=0.1,
+    )
+    parser.add_argument(
+        "--silence-part-speed",
+        "-x",
+        help="If this parameter is set, it will speed up the silence parts x times instead of cutting them out",
+        type=int,
+        required=False,
+        default=1
+    )
+    parser.add_argument(
+        "--min-loud-part-duration",
+        "-l",
+        help="If this parameter is set, loud parts of the clip that are shorter than this parameter "
+             "(in seconds) will also be cutted",
+        type=int,
+        required=False,
+        default=-1,
+    )
+    parser.add_argument(
+        "--codec",
+        help="Codec to use for image encoding. Can be any codec supported by ffmpeg. If the filename "
+             "has extension ‘.mp4’, ‘.ogv’, ‘.webm’, the codec will be set accordingly, but you can still set "
+             "it if you don’t like the default. For other extensions, the output filename must be set accordingly.",
+        type=str,
+        required=False,
+    )
+    parser.add_argument(
+        "--bitrate",
+        help="Desired bitrate for the output video",
+        type=int,
+        required=False,
+    )
+    parser.add_argument(
+        "--default_img", "-d_",
+        help="Path to the inactive status image",
+        type=Path,
+        required=True,
+    )
+    parser.add_argument(
+        "--active_img", "-a_",
+        help="Path to the active status image",
+        type=Path,
+        required=True,
+    )
+    parser.add_argument(
+        "--width", "-w_",
+        help="Desired width for the output video",
+        type=int,
+        required=False,
+    )
+    parser.add_argument(
+        "--height", "-h_",
+        help="Desired height for the output video",
+        type=int,
+        required=False,
+    )
+    parser.add_argument(
+        "--fps",
+        help="Desired fps for the output video",
+        type=int,
+        default=None,
+    )
+
+    args = parser.parse_args()
+    input_path = args.input.resolve()
+    output_path = args.output.resolve()
+    default_img = args.default_img.resolve()
+    active_img = args.active_img.resolve()
+
+    clip = Clip(str(input_path),
+                args.min_loud_part_duration,
+                args.silence_part_speed,
+                str(default_img),
+                str(active_img),
+                args.width,
+                args.height)
+    output = clip.jumpcut(args.magnitude_threshold_ratio,
+                          args.duration_threshold,
+                          args.failure_tolerance_ratio,
+                          args.space_on_edges)
+    output.write_videofile(str(output_path), codec=args.codec, bitrate=args.bitrate, fps=args.fps)
+
+if __name__ == '__main__':
+    main()
---
+++ clip.py
@@ -0,0 +1,133 @@
+import numpy as np
+from PIL import Image
+from moviepy.editor import ImageClip, AudioFileClip, VideoFileClip,CompositeVideoClip, concatenate_videoclips
+from moviepy.video.fx.all import speedx
+from tqdm import tqdm
+import os
+
+class Clip:
+    def __init__(self, clip_path, min_loud_part_duration, silence_part_speed,
+                 default_img, active_img, width, height):
+        self.clip = VideoFileClip(clip_path)
+        image = Image.open(default_img)
+        img_w, img_h = image.size
+        self.img = {
+            "default": default_img,
+            "active": active_img,
+            "width": width if width!=None else img_w,
+            "height": height if width!=None else img_h
+        }
+        Image.new('RGB', (self.img["width"], self.img["height"]), color = (0, 0, 0)).save('BG.png')
+        self.clip = (ImageClip('BG.png').set_duration(self.clip.duration)
+                        .set_audio(AudioFileClip(clip_path))
+                        .set_fps(VideoFileClip(clip_path).fps))
+        os.remove('BG.png')
+        self.default_clip = (ImageClip(self.img["default"])
+                             .set_duration(self.clip.duration)
+                             .resize(width=self.img["width"], height=self.img["height"])
+                             .set_position(("center", "center"))
+                             )
+        self.audio = Audio(self.clip.audio)
+        self.active_clip = ImageClip(self.img["active"])
+
+        self.min_loud_part_duration = min_loud_part_duration
+        self.silence_part_speed = silence_part_speed
+
+
+
+    def get_duration(self):
+        return self.clip.duration
+
+    def jumpcut(
+        self,
+        magnitude_threshold_ratio,
+        duration_threshold_in_seconds,
+        failure_tolerance_ratio,
+        space_on_edges,
+    ):
+
+        intervals_to_cut = self.audio.get_intervals_to_cut(
+            magnitude_threshold_ratio,
+            duration_threshold_in_seconds,
+            failure_tolerance_ratio,
+            space_on_edges,
+        )
+        jumpcutted_clips = self.jumpcut_silent_parts(intervals_to_cut)
+        outputs = concatenate_videoclips(jumpcutted_clips)
+        return outputs
+
+    def jumpcut_silent_parts(self, intervals_to_cut):
+        jumpcutted_clips = []
+        previous_stop = 0
+        for start, stop in tqdm(intervals_to_cut, desc="Cutting silent intervals"):
+            clip_before = (self.clip.subclip(previous_stop, start))
+            clip_before = (CompositeVideoClip([clip_before,
+                          (self.active_clip.set_duration(clip_before.duration)
+                           .resize(width=self.img["width"], height=self.img["height"])
+                           .set_position(("center","center")))]))
+
+            if clip_before.duration > self.min_loud_part_duration:
+                jumpcutted_clips.append(clip_before)
+
+            if self.silence_part_speed is not None:
+                silence_clip = (CompositeVideoClip([self.clip, self.default_clip])
+                                .subclip(start, stop))
+                silence_clip = speedx(
+                    silence_clip, self.silence_part_speed
+                ).without_audio()
+                jumpcutted_clips.append(silence_clip)
+
+            previous_stop = stop
+
+        last_clip = self.clip.subclip(stop, self.clip.duration)
+        jumpcutted_clips.append(last_clip)
+        return jumpcutted_clips
+
+
+class Audio:
+    def __init__(self, audio):
+        self.audio = audio
+        self.fps = audio.fps
+
+        self.signal = self.audio.to_soundarray()
+        if len(self.signal.shape) == 1:
+            self.signal = self.signal.reshape(-1, 1)
+
+    def get_intervals_to_cut(
+        self,
+        magnitude_threshold_ratio,
+        duration_threshold_in_seconds,
+        failure_tolerance_ratio,
+        space_on_edges,
+    ):
+        min_magnitude = min(abs(np.min(self.signal)), np.max(self.signal))
+        magnitude_threshold = min_magnitude * magnitude_threshold_ratio
+        failure_tolerance = self.fps * failure_tolerance_ratio
+        duration_threshold = self.fps * duration_threshold_in_seconds
+        silence_counter = 0
+        failure_counter = 0
+
+        intervals_to_cut = []
+        absolute_signal = np.absolute(self.signal)
+        for i, values in tqdm(
+            enumerate(absolute_signal),
+            desc="Getting silent intervals to cut",
+            total=len(absolute_signal),
+        ):
+            silence = all([value < magnitude_threshold for value in values])
+            silence_counter += silence
+            failure_counter += not silence
+            if failure_counter >= failure_tolerance:
+                if silence_counter >= duration_threshold:
+                    interval_end = (i - failure_counter) / self.fps
+                    interval_start = interval_end - (silence_counter / self.fps)
+
+                    interval_start += space_on_edges
+                    interval_end -= space_on_edges
+
+                    intervals_to_cut.append(
+                        (abs(interval_start), interval_end)
+                    )  # in seconds
+                silence_counter = 0
+                failure_counter = 0
+        return intervals_to_cut
